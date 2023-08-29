4f2b29c015653d0150d54d50dc1ecaa04cf48313
Dmytro Svystun <dmytro.svystun@storable.com>
2023-08-29 22:47:18 +0300

    UC#1 Create filter function by name

---
+++ UseCaseAPI/BusinessLogic/CountryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UseCaseAPI.DAL.Entities;
+
+namespace UseCaseAPI.BusinessLogic
+{
+    public static class CountryExtensions
+    {
+        public static List<Country> FilterByName(this List<Country> countries, string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return countries;
+            }
+
+            return countries.Where(x => x.Name.Common.Contains(name, StringComparison.InvariantCultureIgnoreCase)).ToList();
+        }
+    }
+}
